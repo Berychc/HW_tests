0a4be1ab0dfefaac8b76dfb29ffaef3fcf238308
Berychc <treewincsgo@gmail.com>
2024-01-31 16:06:49 +0300

    Not DOne Homework

--- src/main/java/com/example/calkulator/demo/calculatorService/CalculatorService.java
+++ src/main/java/com/example/calkulator/demo/calculatorService/CalculatorService.java
@@ -1,4 +1,16 @@
 package com.example.calkulator.demo.calculatorService;
 
+import com.example.calkulator.demo.exceptions.DivisionError;
+import com.example.calkulator.demo.exceptions.NoValuesException;
+
 public interface CalculatorService {
+    String welcome();
+
+    int plus(Integer num1, Integer num2) throws NoValuesException, NoValuesException;
+
+    int minus(Integer num1, Integer num2) throws NoValuesException;
+
+    int multiply(Integer num1, Integer num2) throws NoValuesException;
+
+    int divide(Integer num1, Integer num2) throws DivisionError, NoValuesException, DivisionError;
 }
